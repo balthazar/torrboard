87663c35e0385bc07520b1bb871f34157210df11
Balthazar Gronon <bgronon@gmail.com>
2019-11-30 00:01:15 -0800

    lul

--- src/server/fn/deluge.js
+++ src/server/fn/deluge.js
@@ -75,7 +75,11 @@ module.exports = {
           .replace(/ /g, '.')
           .replace(/.s[0-9]+.*/, '')
 
-        const related = files.filter(f => f.toLowerCase() || f.toLowerCase().includes(key))
+        const related = files.filter(
+          f =>
+            f.toLowerCase().includes(data.torrents[id].name.toLowerCase()) ||
+            f.toLowerCase().includes(key),
+        )
         const videos = uniq(
           related.filter(f => ['.mkv', '.avi', '.mp4'].some(ext => f.endsWith(ext))),
         )
