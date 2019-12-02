a805889ccb2648a8c1f275f6e172b501b72e44f0
Balthazar Gronon <bgronon@gmail.com>
2019-12-01 17:58:42 -0800

    Add autodownload cron

--- src/server/fn/downloadRSS.js
+++ src/server/fn/downloadRSS.js
@@ -1,22 +1,38 @@
 const Config = require('../models/Config')
 const getRSS = require('./getRSS')
-const { getDeluge } = require('./deluge')
+const { getDeluge, download } = require('./deluge')
 
 module.exports = async () => {
-  const { autoGrabs } = await Config.findOne()
+  const config = await Config.findOne({})
   const rss = await getRSS()
-  const torrents = await getDeluge()
+  const { torrents } = await getDeluge()
 
-  console.log('autograbs')
+  const autoGrabs = config.autoGrabs.reduce(
+    (acc, cur) => ((acc[cur.toLowerCase()] = true), acc),
+    {},
+  )
 
-  const relevants = []
+  const torrentNames = torrents.reduce((acc, tor) => ((acc[tor.name] = true), acc), {})
+
+  const relevants = {}
 
   rss.forEach(torrent => {
     const { meta } = torrent
-    if (autoGrabs.includes(meta.title) && meta.resolution === '1080p') {
-      relevants.push(torrent)
+    const key = meta.title.toLowerCase()
+    if (autoGrabs[key] && meta.resolution === '1080p') {
+      // Only add if there is none or older
+      if (!relevants[key] || new Date(torrent.date) < new Date(relevants[key].date)) {
+        relevants[key] = torrent
+      }
     }
   })
 
-  console.log(relevants)
+  Object.keys(relevants).forEach(key => {
+    const torrent = relevants[key]
+    if (torrentNames[torrent.title.replace(/ /g, '.')]) {
+      return
+    }
+
+    download({ link: torrent.link })
+  })
 }
