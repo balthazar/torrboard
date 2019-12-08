7813eb4d6052608bc7402316827676c47ed199ec
Balthazar Gronon <bgronon@gmail.com>
2019-12-07 16:58:38 -0800

    Change endpoint url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const __DEV__ = process.env.NODE_ENV !== 'production'
 
 const __APIPORT__ = 3434
-const __APIURL__ = 'https://media.balthazargronon.com/graphql'
+const __APIURL__ = 'https://media.balthazar.dev/graphql'
 
 module.exports = {
   __DEV__,
