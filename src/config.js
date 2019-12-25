66ab309d006dda8968446eff59240af1eb37183e
Balthazar Gronon <bgronon@gmail.com>
2019-12-25 19:05:50 +0100

    Fix some shit

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const __DEV__ = process.env.NODE_ENV !== 'production'
-const BASE_URL = 'https://media.balthazar.dev'
-const DOWNLOAD_DIR = '/home/media'
 
 const __APIPORT__ = 3434
-const __APIURL__ = '/graphql'
+
+const BASE_URL = 'https://media.balthazar.dev'
+const __APIURL__ = `${BASE_URL}/graphql`
 
 module.exports = {
   __DEV__,
@@ -13,6 +13,6 @@ module.exports = {
   BASE_URL,
   IMAGE_URL: `${BASE_URL}/statics/torrboard.png`,
   DOWNLOAD_URL: 'http://balthazargronon.com',
-  DOWNLOAD_DIR,
+  DOWNLOAD_DIR: '/home/media',
   SYSTEM_EMAIL: 'media@balthazar.dev',
 }
