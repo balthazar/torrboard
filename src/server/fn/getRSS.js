f8a096e7122b20d75c64ca7a68c24d3eba923df9
Balthazar Gronon <git@balthazar.dev>
2024-07-03 05:20:25 -0600

    fix(rss): handle back input feed

--- src/server/fn/getRSS.js
+++ src/server/fn/getRSS.js
@@ -6,38 +6,38 @@ const parser = new Parser()
 
 module.exports = async () => {
   const cached = cache.get('rss')
+  if (cached) return cached
 
-  if (cached) {
-    return cached
+  try {
+    const feed = await parser.parseURL(process.env.RSS_URL)
+
+    const res = feed.items.map(item => {
+      const [seeders, leechers] = item.content
+        .match(/^.* - Seeders: ([0-9]+) - Leechers: ([0-9]+)$/)
+        .slice(1)
+
+      const [category] = item.categories
+      const isSerie = category.includes('Episodes')
+
+      const meta = ptn(item.title)
+
+      const { title, link, isoDate: date } = item
+
+      return {
+        title,
+        link,
+        date,
+        category,
+        seeders: Number(seeders),
+        leechers: Number(leechers),
+        meta,
+        isSerie,
+      }
+    })
+
+    cache.put('rss', res, 1e3 * 60)
+    return res
+  } catch (err) {
+    return []
   }
-
-  const feed = await parser.parseURL(process.env.RSS_URL)
-
-  const res = feed.items.map(item => {
-    const [seeders, leechers] = item.content
-      .match(/^.* - Seeders: ([0-9]+) - Leechers: ([0-9]+)$/)
-      .slice(1)
-
-    const [category] = item.categories
-    const isSerie = category.includes('Episodes')
-
-    const meta = ptn(item.title)
-
-    const { title, link, isoDate: date } = item
-
-    return {
-      title,
-      link,
-      date,
-      category,
-      seeders: Number(seeders),
-      leechers: Number(leechers),
-      meta,
-      isSerie,
-    }
-  })
-
-  cache.put('rss', res, 1e3 * 60)
-
-  return res
 }
