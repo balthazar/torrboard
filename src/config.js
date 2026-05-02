401db1d70fabca8a50df0a4b49d7c0c97fd4b228
Balthazar Gronon <git@balthazar.dev>
2026-05-02 17:45:29 -0600

    serve static SPA via express, use env vars for config

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const __DEV__ = process.env.NODE_ENV !== 'production'
 
 const __APIPORT__ = process.env.PORT || 3000
 
-const BASE_URL = 'https://media.balthazar.dev'
+const BASE_URL = process.env.BASE_URL
 const __APIURL__ = `${BASE_URL}/graphql`
 
 module.exports = {
@@ -14,9 +14,9 @@ module.exports = {
 
   BASE_URL,
   IMAGE_URL: `${BASE_URL}/statics/torrboard.png`,
-  DOWNLOAD_URL: 'https://mediafiles.balthazar.dev',
-  DOWNLOAD_DIR: '/home/media',
-  SYSTEM_EMAIL: 'media@balthazar.dev',
+  DOWNLOAD_URL: process.env.DOWNLOAD_URL || `${BASE_URL}/dl`,
+  DOWNLOAD_DIR: process.env.DOWNLOAD_DIR || '/home/media',
+  SYSTEM_EMAIL: process.env.SYSTEM_EMAIL || 'media@balthazar.dev',
 
   TOOLBAR_WIDTH: isMobile ? 50 : 100,
 }
